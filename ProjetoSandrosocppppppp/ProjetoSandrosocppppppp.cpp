a2ff8bbac27a96f32768107f66baaac9eda2d1dc
Philipidev <philipi.altoe@gmail.com>
2020-06-08 22:25:58 -0300

    Primeiro envio

---
+++ ProjetoSandrosocppppppp/ProjetoSandrosocppppppp.cpp
@@ -0,0 +1,45 @@
+// ProjetoSandrosocppppppp.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+
+using namespace std; 
+
+class Agente {
+public:
+    int x = 0;
+    virtual int teste() = 0;
+};
+
+class Mundo : public Agente {
+   
+    //x = 2;
+public:
+    int teste() {
+        cout << "teste" <<endl;
+        return 1;
+    }
+    
+};
+
+int main(int argc, char *argv[])
+{
+    Agente *mundo = new Mundo();
+
+    int x = 0;
+
+    cout << mundo->teste();
+    
+    system("pause");
+}
+
+// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
+// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+
+// Dicas para Começar: 
+//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
+//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
+//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
+//   4. Use a janela Lista de Erros para exibir erros
+//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
+//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
